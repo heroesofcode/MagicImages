0e447e36cd2bae2d2c941812e63a77ca68e5c4a3
joaolfp <joaolucasfp2001@gmail.com>
2023-08-21 21:25:11 -0300

    Add xcodeproject in example

--- Example/Example/SceneDelegate.swift
+++ Example/Example/SceneDelegate.swift
@@ -12,12 +12,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
-        guard let scene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: scene)
-        
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
