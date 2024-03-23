666b57c00d917a6baf170621e175c1d5ccd2d393
joaolfp <joaolucasfp2001@gmail.com>
2024-03-23 20:45:48 -0300

    visionOS Support

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MagicImages",
+    platforms: [.iOS(.v13), .tvOS(.v13), .visionOS(.v1)],
+    products: [
+        .library(
+            name: "MagicImages",
+            targets: ["MagicImages"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "MagicImages",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "MagicImagesTests",
+            dependencies: ["MagicImages"]
+        )
+    ]
+)
